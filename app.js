ee404a0c7f32ea5810ab3ff62852f67a4cea91d5
Rachael Lam <rachael.a.lam@gmail.com>
2021-03-02 17:19:11 -0500

    test

--- app.js
+++ app.js
@@ -10,26 +10,6 @@ function onNavigate(pathname) {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', pathname);
   xhr.send();
-  xhr.onload = function() {
-    if (xhr.status != 200) {
-      alert(`Error ${xhr.status}: ${xhr.statusText}`);
-    }
-    else {
-      alert(`Done, got ${xhr.response.length} bytes`);
-    }
-};
-
-xhr.onprogress = function(event) {
-  if (event.lengthComputable) {
-    alert(`Received ${event.loaded} of ${event.total} bytes`);
-  } else {
-    alert(`Received ${event.loaded} bytes`); // no Content-Length
-  }
-
-};
-
-xhr.onerror = function() {
-  alert("Request failed");
 };
 
 window.onpopstate = () => {
