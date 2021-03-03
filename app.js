9d0b5bd9ca730adacbba9aad133a528797999e72
Rachael Lam <rachael.a.lam@gmail.com>
2021-03-03 11:36:53 -0500

    java updates

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const rootDiv = document.getElementById('root');
-var client = new XMLHttpRequest();
-client.open('GET', 'index.html');
-client.onreadystatechange = function() {
-  rootDiv.innerHTML = client.responseText;
-}
-client.send();
-
 window.onpopstate = () => {
     var client = new XMLHttpRequest();
     client.open('GET', 'home.html');
@@ -32,24 +24,4 @@ function resize() {
     }
 }
 
-window.onresize = resize;
-
-
-function changeShape(type) {
-    if (type == 'student') {
-        var x = document.getElementById("shape");
-        x.style.backgroundImage = "linear-gradient(45deg, #F8333C 0%, #FCAB10 100%)";
-    } else if (type == 'developer') {
-        var x = document.getElementById("shape");
-        x.style.backgroundImage = "linear-gradient(45deg, #38AECC 0%, #35A7FF 100%)";
-    } else if (type == 'designer') {
-        var x = document.getElementById("shape");
-        x.style.backgroundImage = "linear-gradient(45deg, #0595e5 0%, #0595e5 100%)";
-    }
-}
-
-function returnShape() {
-    var x = document.getElementById("shape");
-    x.style.backgroundImage = "none";
-    x.style.backgroundImage = "linear-gradient(45deg, #81F499 0%, #6DD3CE 100%)";
-}
+window.onresize = resize();
